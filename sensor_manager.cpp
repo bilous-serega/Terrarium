371ffa35978f44d80456dec25a99a1f13699c48c
bilous-serega <bilous.serega@gmail.com>
2026-05-11 12:51:00 +0300

    Add files via upload

--- sensor_manager.cpp
+++ sensor_manager.cpp
@@ -1,22 +1,62 @@
 #include "sensor_manager.h"
+#include "pins.h"
+#include "globals.h"
+#include "config.h"
 
-static float currentTemperature = 0.0;
-static float currentHumidity = 0.0;
+#include <DHT.h>
+#include <math.h>
+
+#define DHT_TYPE DHT22
+
+static DHT dht(PIN_DHT, DHT_TYPE);
 
 static bool sensorValid = false;
 
+static unsigned long lastReadTime = 0;
+
 void initSensors()
 {
+    dht.begin();
+
     sensorValid = false;
 
-    currentTemperature = 0.0;
-    currentHumidity = 0.0;
+    climate.temperature = 0.0;
+    climate.humidity = 0.0;
+
+    Serial.println("DHT22 INIT");
 }
 
 void updateSensors()
 {
-    // Placeholder
-    // Real sensor logic later
+    if (millis() - lastReadTime < 2000)
+        return;
+
+    lastReadTime = millis();
+
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
+
+    if (isnan(temperature) || isnan(humidity))
+    {
+        sensorValid = false;
+
+        Serial.println("DHT READ ERROR");
+
+        return;
+    }
+
+    climate.temperature = temperature;
+    climate.humidity = humidity;
+
+    sensorValid = true;
+
+    #if DEBUG_SENSORS
+        Serial.print("TEMP: ");
+        Serial.println(climate.temperature);
+
+        Serial.print("HUM: ");
+        Serial.println(climate.humidity);
+    #endif
 }
 
 bool isSensorValid()
@@ -26,10 +66,10 @@ bool isSensorValid()
 
 float getTemperature()
 {
-    return currentTemperature;
+    return climate.temperature;
 }
 
 float getHumidity()
 {
-    return currentHumidity;
+    return climate.humidity;
 }
